d86e38e279376f60b9103d7f29d512d948667b13
unknown <QA@.cerego.co.jp>
2009-05-08 01:51:21 +0900

    add basic commands

--- Jot.java
+++ Jot.java
@@ -1,18 +1,39 @@
 import java.io.*;
-
+import java.util.*;
+import java.lang.reflect.*;
 class Jot {
 		Joint joint;
+		Iterator i,ii;
 		public Jot(){
 			joint = new Joint("");
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			while (true){
-                int i = 0;
-                System.out.print("Jot:$");
+                System.out.print(joint.getName()+":$");
                 try {
 					String s = br.readLine();
-					if (s.equals("list")){
-					}else if (s.equals("show")){
+					if (s.equals("ls")){
+						/*
+						List a = Arrays.asList(System.getProperty("java.class.path").split(System.getProperty("path.separator")));
+				        for (i = a.iterator(); i.hasNext();) {
+				            String ss = (String)i.next();
+							File f = new File(ss);
+							if (f.isDirectory()){
+								List aa  = Arrays.asList(f.list());
+						        for (ii = aa.iterator(); ii.hasNext();) {
+						            String sss = (String)ii.next();
+									System.out.println(sss);					        	
+						        }
+							}
+				        }
+*/
+				        for (i = joint.list().iterator(); i.hasNext();) {
+				            Method m = (Method)i.next();
+				        	System.out.println(m.getName()); //変更がList側にも反映
+				        }
+        			}else if (s.equals("show")){
 						System.out.println(joint.getName());
+        			}else if (s.equals("path")){
+						System.out.println(System.getProperty("java.class.path"));
 					}else if (s.equals("")){
 						System.out.println(joint.getName());
 					}else if (s.equals("exit")){
@@ -37,10 +58,15 @@ class Joint{
 			ClassLoader classLoader = this.getClass().getClassLoader();
 			_class = classLoader.loadClass(s);
 		}catch(Throwable th){
-			_class = this.getClass();
+			if (_class == null){
+				_class = this.getClass();
+			}
 		}
 	}
     public String getName(){
 		return _class.getName();
+	}
+	public List list(){
+		return java.util.Arrays.asList(_class.getDeclaredMethods());		
 	}
 }
